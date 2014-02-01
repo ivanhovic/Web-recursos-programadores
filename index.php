161e4ea55b1ee65ea37ef1417daebdb11ab5e6a5
Ivan Murillo <murilloivan@gmail.com>
2014-02-01 14:16:57 +0100

    new version

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>CodeLab</title>
-
+    <!-- STYLES COMPLETE -->
     <link href="css/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <link href="css/dist/css/custom.css" rel="stylesheet">
-    <link rel="stylesheet" href="font-mfizz-1.2/font-mfizz.css">
     <link href="css/dist/css/bootstrap-theme.min.css" rel="stylesheet" media="screen"> 
-    
+    <!-- CUSTOM CSS -->
+    <link href="css/dist/css/custom.css" rel="stylesheet">
+    <!-- ICONOS -->
     <link rel="stylesheet" href="font-awesome-4.0.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="font-mfizz-1.2/font-mfizz.css">
 
   </head>
 
@@ -18,31 +18,48 @@
 
 <!-- =============== NAVBAR ============ -->
 
-    <nav id="top" class="navbar navbar-inverse">
+    <div class="navbar navbar-inverse" role="navigation">
       <div class="container">
-        <a class="navbar-brand" href="index.html"><i class="icon-objc" style="font-size:2em"></i> CodeLab</a>
-          <!--<div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav pull-right">
-              <li><a href="info.html">Info</a></li>
-            </ul>
-          </div>  -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">CodeLab</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="#">Inicio</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contacto</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
       </div>
-    </nav>
+    </div>
 
 <!-- =============== CONTENIDO =================== -->
 
-    <div class="container">
 
       <div class="jumbotron">
+        <div class="container">
+          <div class="col-lg-8">
         <h1>¡Conviértete en programador!</h1>
-        <p class="lead">Si has llegado aquí, seguramente estés interesado en aprender a programar o simplemente, en perfeccionar lo que ya sabes. Aqui tienes a tu disposición una <b>recopilación</b> de los mejores recursos online.</p>
-        <iframe width="560" height="315" src="//www.youtube.com/embed/nKIu9yen5nc" frameborder="0" allowfullscreen></iframe>
+        <p>Aprende a programar o perfecciona lo que ya sabes. Aqui tienes a tu disposición una <b>recopilación</b> de los mejores recursos online.</p>
         <p><a class="btn btn-lg btn-success" href="#empieza" role="button">¡Empieza!</a></p>
           <!--<p>Si no tienes ni idea y quieres sumerguirte poco a poco, próximamente tendrás una guía <a href="aperitivo.html">básica</a>,
            otra <a href="almuerzo.html">intermedia</a> y una última más <a href="postre.html">avanzada</a>.</p> -->
       </div>
-      <div class="container marketing">
+      <div class="col-lg-4">
+        <i class="fa fa-keyboard-o" style="font-size:8em"></i>
+      </div>
+      </div>
+    </div>
 
+       <div class="container">
+      <div class="marketing">
+        <!--<div class="container">-->
         <!-- Tres columnas de info -->
 
         <div id="empieza" class="row">
@@ -61,27 +78,30 @@
           <div class="col-lg-4">
             <i class="fa fa-mobile" style="font-size:10em"></i>
             <h2>Móviles</h2>
-            <p>Seguro que desde hace tiempo, tienes pensada una app en a cebeza? Aqui tienes ayuda para empezar a desarrollar para iPhone, Android o Firefox.</p>
+            <p>Seguramente ya tienes una idea. Aqui tienes ayuda para empezar a desarrollar la app tanto para iPhone o Android.</p>
             <p><a class="btn btn-default" href="moviles.html" role="button">Detalles &raquo;</a></p>
           </div>
         </div><!-- /.row -->
+      <!--</div>-->
+
+    </div> <!-- /container -->
 
 <!-- ================= FOOTER =========== -->
 
         <div class="footer">
           <div class="container">
-             <p class="pull-left"><a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0" src="http://i.creativecommons.org/l/by/4.0/88x31.png" /></a>. &middot; <a href="#">Contacto</a></p>
+             <p class="pull-left"><a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0" src="http://i.creativecommons.org/l/by/4.0/88x31.png" /></a>.</p>
              <p class="pull-right"><a href="#top">Back to top</a></p>
           </div>
         </div>
       </div>
-    </div> <!-- /container -->
+
 
       <!-- ================================================== -->
-      <!--  JavaScript al final para que la web cargue antes   
+      <!--  JavaScript al final para que la web cargue antes  --> 
       <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
       <script src="dist/js/bootstrap.min.js"></script>
-      <script src="../../docs-assets/js/holder.js"></script> -->
+      <script src="../../docs-assets/js/holder.js"></script>
 
 
   </body>
