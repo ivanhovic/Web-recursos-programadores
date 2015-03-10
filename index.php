e50dc0932e9f4dc9a1e7e1f8890117ad239a2543
Ivan Murillo <murilloivan@gmail.com>
2015-03-10 20:04:09 +0100

    form index mail chimp

--- index.php
+++ index.php
@@ -104,13 +104,13 @@
             
             <!-- Begin MailChimp Signup Form -->  
 
-                <form action="//codealo.us10.list-manage.com/subscribe/post?u=bcbe5d1ddcf0a8857f87b302e&amp;id=00aa27e232" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                  
+                <form action="//codealo.us10.list-manage.com/subscribe/post?u=bcbe5d1ddcf0a8857f87b302e&amp;id=00aa27e232" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate azul form-inline" target="_blank" novalidate>
+                  <p>Sign up to know when we launch</p>
                   <div class="form-group">
                     <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
                   </div>
                   <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                  <input type="text" name="b_bcbe5d1ddcf0a8857f87b302e_00aa27e232" tabindex="-1" value="">
+                  
                   <input type="submit" value="Suscríbeme" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary">
 
                 </form>
